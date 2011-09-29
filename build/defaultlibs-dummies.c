eda55e7712d550c0c1f585c3c841effc990e78c1
robbesol <github@robbesol.net>
2011-09-29 11:15:09 +0000

    cleaned dummies for checking library externals

--- build/defaultlibs-dummies.c
+++ build/defaultlibs-dummies.c
@@ -38,66 +38,58 @@
 
 #define WEAKDEF(decl)	decl __attribute__ ((weak));; decl { /* dummy body */ }
 
-struct xprintf_channel;
-struct xprintf_protectedChannel;
-
-WEAKDEF(int xprintf_beginProtectedIO(struct xprintf_channel *xch,
-				struct xprintf_protectedChannel *xchpr))
-WEAKDEF(int xprintf_endProtectedIO(struct xprintf_channel *xch,
-				struct xprintf_protectedChannel *xchpr))
+//struct xprintf_channel;
+//struct xprintf_protectedChannel;
+//
+//WEAKDEF(int xprintf_beginProtectedIO(struct xprintf_channel *xch,
+//				struct xprintf_protectedChannel *xchpr))
+//WEAKDEF(int xprintf_endProtectedIO(struct xprintf_channel *xch,
+//				struct xprintf_protectedChannel *xchpr))
 
 //#include <ctype.h>
 //const char *__ctype_ptr; // macros in <ctype.h> need this global variable
 //const char *__ctype_ptr__; // same for gcc 4.3.2
 
 #include <string.h> // for memset()
-// <string.h> strl*(), not standard c, but we need them anyway
-//WEAKDEF(size_t strlcat(char *str1, const char *str2, size_t len))
-//WEAKDEF(size_t strlcpy(char *str1, const char *str2, size_t len))
-// <string.h> str*() that are needed:
-//WEAKDEF(size_t strlen(const char *str))
-//WEAKDEF(int strcmp(const char *str1, const char *str2))
-
-// <string.h> str*(), not yet needed:
-//WEAKDEF(char *strchr(const char *str, int i))
-//WEAKDEF(char *strrchr(const char *str, int i))
-//WEAKDEF(char *strstr(const char *str1, const char *str2))
-
-// I sincerely hope these are not needed.
-// If you need to ask why, please find yourself another profession.
-// char *strncat(char *str1, const char *str2, size_t len);
-// int strncmp(const char *str1, const char str2*, size_t len);
-// char *strncpy(char *str1, const char *str2, size_t len);
 
 // <string.h> mem*() that are needed:
-//WEAKDEF(int memcmp(const void *p1, const void *p2, size_t len))
 #ifdef memset
 #undef memset
 #endif
 WEAKDEF(void *memset(void *p, int val, size_t len))
+
+//WEAKDEF(int memcmp(const void *p1, const void *p2, size_t len))
 //WEAKDEF(void *memcpy(void *p1, const void *p2, size_t size))
 
 // <string.h> mem*(), not yet needed:
 //WEAKDEF(void *memchr(const void *, int, size_t))
 //WEAKDEF(void *memmove(void *, const void *, size_t))
 
+// <string.h> strl*(), not standard c, but we need them anyway
+//WEAKDEF(size_t strlcat(char *str1, const char *str2, size_t len))
+//WEAKDEF(size_t strlcpy(char *str1, const char *str2, size_t len))
+
+				// <string.h> str*() that are needed:
+//WEAKDEF(size_t strlen(const char *str))
+//WEAKDEF(int strcmp(const char *str1, const char *str2))
+
 #ifndef XPRINTF_NO_DOUBLES
 
 #include <math.h>
-WEAKDEF(int __fpclassifyf(float x))
-// needed for fpclassify() macro
-WEAKDEF(int __fpclassifyd(double x))
-// needed for fpclassify() macro
-WEAKDEF(int __signbitf (float x))
-// for signbit() macro
-WEAKDEF(int __signbitd (double x))
-// for signbit() macro
-WEAKDEF(double modf(double d, double *dp))
+
 WEAKDEF(double fmod(double d1, double d2))
-WEAKDEF(double log10(double d))
 WEAKDEF(double floor(double d))
 WEAKDEF(double ceil(double d))
-
 WEAKDEF(double fabs(double x))
+WEAKDEF(double log10(double d))
+WEAKDEF(double modf(double d, double *dp))
+
+// needed for fpclassify() macro
+WEAKDEF(int __fpclassifyf(float x))
+WEAKDEF(int __fpclassifyd(double x))
+// for signbit() macro
+
+WEAKDEF(int __signbitf (float x))
+WEAKDEF(int __signbitd (double x))
 
 #endif /* ! XPRINTF_NO_DOUBLES */
