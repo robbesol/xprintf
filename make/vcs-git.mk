8d26b027de292a91b16b9c5c1342021a09b60555
robbesol <github@robbesol.net>
2011-11-02 18:04:48 +0100

    better GIT strings in doc

--- make/vcs-git.mk
+++ make/vcs-git.mk
@@ -5,18 +5,31 @@ ifeq (,$(strip $(PROJECT_TOP_DIR)))
 $(error PROJECT_TOP_DIR not set)
 endif
 
-_GITID_INFO_URL := $(strip $(shell cd $(PROJECT_TOP_DIR) && git config --get remote.origin.url))
+_GITID_INFO_URL := $(strip $(shell cd $(PROJECT_TOP_DIR) && git config --get remote.origin.url | sed -e 's|//[^@/]*@|//|'))
 ifneq (,$(strip $(_GITID_INFO_URL)))
 VCS_URL := $(_GITID_INFO_URL)
 endif
 
-_GITID_REMOTES_VERSION := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --remotes --format=format:'%H')
-_GITID_LOCAL_VERSION := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --format=format:'%H')
+_GITID_VERSION_LONG_REMOTE := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --remotes --format=format:'%H')
+_GITID_VERSION_SHORT_REMOTE := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --remotes --format=format:'%h')
+_GITID_VERSION_LONG_LOCAL := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --format=format:'%H')
+_GITID_VERSION_SHORT_LOCAL := $(shell cd $(PROJECT_TOP_DIR) && git log -1 --format=format:'%h')
 
-ifneq (,$(strip $(_GITID_REMOTES_VERSION)))
-VCS_VERSION := $(_GITID_REMOTES_VERSION)
+ifneq (,$(strip $(_GITID_VERSION_LONG_REMOTE)))
+VCS_VERSION_LONG := $(_GITID_VERSION_LONG_REMOTE)
+endif
+ifneq (,$(strip $(_GITID_VERSION_SHORT_REMOTE)))
+VCS_VERSION := $(_GITID_VERSION_SHORT_REMOTE)
 endif
 
-ifneq ($(strip $(_GITID_LOCAL_VERSION)),$(strip $(_GITID_REMOTES_VERSION)))
-VCS_VERSION += + local $(_GITID_LOCAL_VERSION)
+ifneq ($(strip $(_GITID_VERSION_LONG_LOCAL)),$(strip $(_GITID_VERSION_LONG_REMOTE)))
+VCS_VERSION_LONG += + local $(_GITID_VERSION_LONG_LOCAL)
+endif
+ifneq ($(strip $(_GITID_VERSION_SHORT_LOCAL)),$(strip $(_GITID_VERSION_SHORT_REMOTE)))
+VCS_VERSION += + local $(_GITID_VERSION_SHORT_LOCAL)
+endif
+
+VCS_VERSION_FULL := $(strip $(VCS_URL))
+ifneq (,$(strip $(VCS_VERSION_LONG)))
+VCS_VERSION_FULL += commit $(strip $(VCS_VERSION_LONG))
 endif
--- make/vcs.mk
+++ make/vcs.mk
@@ -8,8 +8,17 @@ ifeq (,$(strip $(PROJECT_TOP_DIR)))
 $(error PROJECT_TOP_DIR not set)
 endif
 
+# If you use a VCS method XXX we expeect a makefile vcs-XXX.mk in the directory of the makefile you are now reading. 
+# That makefile should set the following variables: 
+# VCS_URL -- the URL of the source repository (no default)
+# VCS_VERSION == the "normal" version string (no default)
+# VCS_VERSION_LONG -- the "long" version string (default: VCS_VERSION)
+# VCS_VERSION_FULL -- the full version string, combining the URL and version (default: VCS_URL VCS_VERSION_LONG)
+
 VCS_URL :=
 VCS_VERSION :=
+VCS_VERSION_LONG :=
+VCS_VERSION_FULL :=
 
 # first, do everything involving MAKEFILE_LIST: it changes with each "include"
 _VCS_MKFILE_PATH := $(lastword $(MAKEFILE_LIST))
@@ -20,3 +29,10 @@ $(warning No makefile file VCS $(VERSION_CONTROL_SYSTEM): expected $(_VCS_X_MK_F
 endif
 
 -include $(_VCS_X_MK_FILE)
+
+ifeq (,$(strip $(VCS_VERSION_LONG)))
+VCS_VERSION_LONG := $(strip $(VCS_VERSION))
+endif
+ifeq (,$(strip $(VCS_VERSION_FULL)))
+VCS_VERSION_FULL := $(strip $(VCS_URL) $(VCS_VERSION_LONG))
+endif
